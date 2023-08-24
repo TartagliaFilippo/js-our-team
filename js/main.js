2f1ef91fb3acbc55d0d6e49d51ba86eb7fefd2b1
filippo199600 <pippotartaglia@gmail.com>
2023-08-24 15:22:15 +0200

    feat milestone 1

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// creo un array di oggetti
+const team = [
+  {
+    name: "Wayne Barnett",
+    task: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    task: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    task: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    task: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    task: "Developer",
+    photo: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    task: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.table(team);
